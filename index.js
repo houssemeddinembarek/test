b705a67516ddf2f2c4df8754d2aef806f0b5d69a
houssemeddinembarek <houssemeddinembarek@gmail.com>
2023-02-09 11:39:45 +0100

    commit 2

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 var bodyParser = require("body-parser");
-
-
 const app = express();
+require("express-ws")(app);
+
 app.use(express.json({ extended: false }));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = process.env.PORT || 3002;
+// const port = process.env.PORT || 3002;
 
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
-
-app.listen(port, "0.0.0.0", () => {
-
-//   console.log("connected to the backend  !");
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+app.ws("/echo", (ws, res) => {
+  ws.on("message", (msg) => {
+    ws.send(msg);
+  });
+});
+
+app.listen(3002);
